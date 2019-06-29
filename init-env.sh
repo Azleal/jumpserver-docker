fccc8860c90d90bb0ea036bc7f6a3e3a2468ec77
liyao.mr <liyao.mr@gmail.com>
2019-06-29 17:37:59 +0800

    change script

--- init-env.sh
+++ init-env.sh
@@ -6,13 +6,13 @@ stack_name=jump
 cd "$current_dir"
 #create secrets configs, which will be used as secrets file in docker-compose later on.
 mkdir -p secrets && rm -rf secrets/*
-docker stack rm jumpserver
+docker stack rm ${stack_name}
 ./secrets-generator/generate-secrets.sh && cp -r ./secrets-generator/secrets/* ./secrets/
 echo building images...
 docker-compose build
 echo images have been built...
 echo deploying jumpserver...
-docker stack deploy -c docker-compose.yml ${stack_name}
+docker stack deploy -c docker-compose.yml --resolve-image always --with-registry-auth ${stack_name}
 if [ "$?" == "0" ]; then
     echo jumpserver deployed, visit http://localhost
 fi
